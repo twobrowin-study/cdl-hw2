ac8cbae10a1e6ebb59e10af96ab76ef500410fc7
Dubrovin Egor <dubrovin.en@gmail.com>
2017-05-13 14:26:57 +0300

    Update make_run.sh

--- make_run.sh
+++ make_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$(uname)" == "Linux" ]; then
   gcc -o ./main main.c errors/errors_helper.c errors/errors_helper.h token_scan/token_scan.c token_scan/token_scan.h token_scan/token_scan_helpers.c token_scan/token_scan_helpers.h syntax_analyzer/syntax_analyzer.c syntax_analyzer/syntax_analyzer.h syntax_analyzer/syntax_analyzer_helpers.c syntax_analyzer/syntax_analyzer_helpers.h
-  ./main "$1";
+  ./main "$1"
 fi
 
 if [ "$(uname)" == "MINGW32_NT-10.0" ]; then
