f40121dc26d9a34cb20e110b9336f1e4674aadab
Dubrovin Egor <dubrovin.en@gmail.com>
2017-05-13 14:25:59 +0300

    Add MINGW script

--- make_run.sh
+++ make_run.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-./make.sh && ./main "$1"
+#!/bin/bash
+if [ "$(uname)" == "Linux" ]; then
+  gcc -o ./main main.c errors/errors_helper.c errors/errors_helper.h token_scan/token_scan.c token_scan/token_scan.h token_scan/token_scan_helpers.c token_scan/token_scan_helpers.h syntax_analyzer/syntax_analyzer.c syntax_analyzer/syntax_analyzer.h syntax_analyzer/syntax_analyzer_helpers.c syntax_analyzer/syntax_analyzer_helpers.h
+  ./main "$1";
+fi
+
+if [ "$(uname)" == "MINGW32_NT-10.0" ]; then
+  mingw32-gcc -o ./main.exe main.c errors/errors_helper.c errors/errors_helper.h token_scan/token_scan.c token_scan/token_scan.h token_scan/token_scan_helpers.c token_scan/token_scan_helpers.h syntax_analyzer/syntax_analyzer.c syntax_analyzer/syntax_analyzer.h syntax_analyzer/syntax_analyzer_helpers.c syntax_analyzer/syntax_analyzer_helpers.h
+  ./main.exe "$1"
+fi
