e083a2414f9e94861aab964feff9cce776707774
Dubrovin Egor <dubrovin.en@gmail.com>
2017-05-31 20:51:58 +0300

    Create make_run_termux.sh

---
+++ make_run_termux.sh
@@ -0,0 +1,3 @@
+#README: to run `sh make_run_termux.sh`
+gcc main.c errors/errors_helper.c errors/errors_helper.h token_scan/token_scan.c token_scan/token_scan.h token_scan/token_scan_helpers.c token_scan/token_scan_helpers.h syntax_analyzer/syntax_analyzer.c syntax_analyzer/syntax_analyzer.h syntax_analyzer/syntax_analyzer_helpers.c syntax_analyzer/syntax_analyzer_helpers.h
+./a.out "$1"
