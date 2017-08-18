4fb8a6bc4bab93c95c5fbfeebf30d76c8e5d09fb
Dubrovin Egor <dubrovin.en@gmail.com>
2017-08-18 12:07:05 +0300

    Changed win version to cygwin

--- make_run.sh
+++ make_run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+gcc -o ./main main.c errors/errors_helper.c errors/errors_helper.h token_scan/token_scan.c token_scan/token_scan.h token_scan/token_scan_helpers.c token_scan/token_scan_helpers.h syntax_analyzer/syntax_analyzer.c syntax_analyzer/syntax_analyzer.h syntax_analyzer/syntax_analyzer_helpers.c syntax_analyzer/syntax_analyzer_helpers.h
 if [ "$(uname)" == "Linux" ]; then
-  gcc -o ./main main.c errors/errors_helper.c errors/errors_helper.h token_scan/token_scan.c token_scan/token_scan.h token_scan/token_scan_helpers.c token_scan/token_scan_helpers.h syntax_analyzer/syntax_analyzer.c syntax_analyzer/syntax_analyzer.h syntax_analyzer/syntax_analyzer_helpers.c syntax_analyzer/syntax_analyzer_helpers.h
   ./main "$1"
-elif [ "$(uname)" == "MINGW32_NT-10.0" ] || [ "$(uname)" == "MSYS_NT-10.0" ]; then
-  mingw32-gcc -o ./main.exe main.c errors/errors_helper.c errors/errors_helper.h token_scan/token_scan.c token_scan/token_scan.h token_scan/token_scan_helpers.c token_scan/token_scan_helpers.h syntax_analyzer/syntax_analyzer.c syntax_analyzer/syntax_analyzer.h syntax_analyzer/syntax_analyzer_helpers.c syntax_analyzer/syntax_analyzer_helpers.h
+elif [ "$(uname)" == "CYGWIN_NT-10.0" ]; then
   ./main.exe "$1"
 fi
